31d93c67b2fce8411c90f4b96bca39b0e97e08d3
Erik van Sebille <erikvansebille@gmail.com>
2026-03-07 14:00:35 +0100

    One more change for ColorMode toggle

--- src/theme.js
+++ src/theme.js
@@ -2,12 +2,6 @@ import { extendTheme } from '@chakra-ui/react'
 import { theme } from '@chakra-ui/theme'
 import '@fontsource-variable/inter'
 
-// color mode config
-const config = {
-  initialColorMode: 'light',
-  useSystemColorMode: true,
-}
-
 const fonts = {
   body: "'Inter Variable', -apple-system, BlinkMacSystemFont, 'Segoe UI', Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'",
   heading: "'Champ Black', 'Inter Variable', 'Work Sans', system-ui, sans-serif",
@@ -16,7 +10,6 @@ const fonts = {
 export const customTheme = extendTheme(
   {
     colors: { ...theme.colors, brand: theme.colors.blue },
-    config: config,
     fonts: fonts,
     components: {
       Text: {
