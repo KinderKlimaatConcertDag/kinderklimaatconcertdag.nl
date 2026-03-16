78e709324c6937a8939d9f5a8dbebc107f61396f
Erik van Sebille <erikvansebille@gmail.com>
2026-03-16 21:42:28 +0100

    Add children to media menu bar item

--- src/data/menu-items.js
+++ src/data/menu-items.js
@@ -2,7 +2,15 @@ export const menuItems = [
   { label: 'Editie 2026', href: '/#editie2026' },
   { label: 'Hoe het begon', href: '/#hoehetbegon' },
   { label: 'Partners', href: '/#partners' },
-  { label: 'Media', href: '/resources#media' },
+  {
+    label: 'Media',
+    href: '/resources#media',
+    children: [
+      { label: 'Media', href: '/resources#media' },
+      { label: 'Artwork', href: '/resources#artwork' },
+      { label: 'Foto\'s', href: '/resources#fotos' },
+    ],
+  },
   { label: 'Team', href: '/overons' },
   {
     label: 'Stichting',
