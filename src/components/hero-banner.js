4e64e73b64b9db50ab4608964368fa406a513b64
Erik van Sebille <erikvansebille@gmail.com>
2026-04-19 13:28:30 +0200

    Adding frontpage images

--- src/components/hero-banner.js
+++ src/components/hero-banner.js
@@ -31,6 +31,25 @@ export const HeroBanner = () => {
         </Box>
       </Box>
       <Container maxW='container.lg' pb={12}>
+        <Box display='flex' justifyContent='center' gap={{ base: 4, md: 8 }} mt={{ base: '-40px', md: '-60px' }} position='relative' zIndex={1} alignItems='flex-start'>
+          <Image
+            src='/assets/KKCD_Site_Schets_014.png'
+            alt='Kinder Klimaat Concert Dag'
+            w={{ base: '300px', md: '400px' }}
+          />
+          <Image
+            src='/assets/KKCD_Site_Schets_012.png'
+            alt='EDITIE #1 - ZO 27 SEP 2026 - TivoliVredenburg Utrecht'
+            w={{ base: '200px', md: '300px' }}
+          />
+          <Image
+            src='/assets/KKCD_Site_Schets_015.png'
+            alt='Bij'
+            w={{ base: '200px', md: '300px' }}
+            mt={{ base: '30px', md: '50px' }}
+            ml={{ base: '-50px', md: '-80px' }}
+          />
+        </Box>
         <Text py={5}>
           <Text as='span' textStyle={'paragraphBlock'}>
             Tijdens de Kinder Klimaat Concert Dag staat het perspectief van kinderen en jongeren op klimaatverandering centraal.
