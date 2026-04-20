23a25a0a7fc597373a276e1a246079c33bfda918
Erik van Sebille <erikvansebille@gmail.com>
2026-04-20 21:08:58 +0200

    Update order and positioning of figures for small screens

--- src/components/hero-banner.js
+++ src/components/hero-banner.js
@@ -3,13 +3,14 @@ import { Image } from '@/components/mdx'
 
 export const HeroBanner = () => {
   return (
-    <Box as='section' bg='#EEEEEE'>
+    <Box as='section' bg='#EEEEEE' overflowX='hidden'>
       <Box w='100%' pt={'60px'}>
         <Box position='relative'>
           <Image
             src={'/assets/KKCD_Site_Schets_01.png'}
             alt='Kinder Klimaat Concert Dag'
             w='100%'
+            h={{ base: '200px', md: 'auto' }}
             objectFit='cover'
           />
           <Text
@@ -17,7 +18,7 @@ export const HeroBanner = () => {
             top='50%'
             left='50%'
             transform='translate(-50%, -50%)'
-            fontSize={{ base: 'xl', sm: '2xl', md: '5xl', lg: '7xl' }}
+            fontSize={{ base: '2xl', md: '5xl', lg: '7xl' }}
             fontFamily={'heading'}
             fontWeight='bold'
             color='white'
@@ -31,25 +32,31 @@ export const HeroBanner = () => {
         </Box>
       </Box>
       <Container maxW='container.lg' pb={12}>
-        <Box display='flex' justifyContent='flex-start' gap={{ base: 4, md: 8 }} mt={{ base: '-40px', md: '-60px' }} position='relative' zIndex={1} alignItems='flex-start'>
+        <Box display='flex' justifyContent={{ base: 'center', md: 'flex-start' }} flexWrap={{ base: 'wrap', md: 'nowrap' }} gap={{ base: 4, md: 8 }} mt={{ base: '-40px', md: '-60px' }} position='relative' zIndex={1} alignItems='flex-start'>
           <Image
             src='/assets/KKCD_Site_Schets_014.png'
             alt='Kinder Klimaat Concert Dag'
-            w={{ base: '300px', md: '400px' }}
-            mt = {{ base: '60px', md: '80px' }}
+            w={{ base: '400px', sm: '400px' }}
+            mt={{ base: '60px', md: '80px' }}
           />
-          <Image
-            src='/assets/KKCD_Site_Schets_012.png'
-            alt='EDITIE #1 - ZO 27 SEP 2026 - TivoliVredenburg Utrecht'
-            w={{ base: '200px', md: '300px' }}
-          />
-          <Image
-            src='/assets/KKCD_Site_Schets_015.png'
-            alt='Bij'
-            w={{ base: '200px', md: '300px' }}
-            mt={{ base: '30px', md: '50px' }}
-            ml={{ base: '-50px', md: '-80px' }}
-          />
+          <Box display='flex' gap={{ base: 4, md: 8 }} alignItems='flex-start' minW={0}>
+            <Image
+              src='/assets/KKCD_Site_Schets_012.png'
+              alt='EDITIE #1 - ZO 27 SEP 2026 - TivoliVredenburg Utrecht'
+              w={{ base: '60%', md: '300px' }}
+              maxW='300px'
+              flexShrink={1}
+            />
+            <Image
+              src='/assets/KKCD_Site_Schets_015.png'
+              alt='Bij'
+              w={{ base: '45%', md: '300px' }}
+              maxW='300px'
+              mt={{ base: '30px', md: '50px' }}
+              ml={{ base: '-40px', sm: '-50px', md: '-80px' }}
+              flexShrink={1}
+            />
+          </Box>
         </Box>
         <Box py={5}>
           <Text
