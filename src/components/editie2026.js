bbecba2370e4d6c4441ebe2e2428494c328e56ba
Erik van Sebille <erikvansebille@gmail.com>
2026-06-08 17:01:44 +0200

    Adding links to editie2026

--- src/components/editie2026.js
+++ src/components/editie2026.js
@@ -1,4 +1,4 @@
-import { Box, Container, Divider, Text } from '@chakra-ui/react'
+import { Box, Container, Divider, Link, Text } from '@chakra-ui/react'
 import { Heading, Image } from '@/components/mdx'
 
 export const Editie2026 = () => {
@@ -36,7 +36,11 @@ export const Editie2026 = () => {
               Jonge musici van circa 10 tot 25 jaar staan centraal en treden op in wisselende bezettingen. Veel van hen zijn verbonden aan onze partnerorganisaties, de Nationale JeugdOrkesten Nederland, en de Jong talentafdeling van het Koninklijk Conservatorium Den Haag.
             </Text>
             <Text as='span' textStyle={'paragraphBlock'}>
-              Door muziek uit verschillende periodes en stijlen te combineren, ontstaat een veelzijdig geheel. Zo mogen de Vier Jaargetijden van Vivaldi natuurlijk niet ontbreken, maar wat dacht je bijvoorbeeld ook van het minder bekende Winter van Astor Piazzolla of Gershwin's Summertime uit Porgy and Bess? Daarnaast gaat de Kindercomponist des Vaderlands Kobus Jansen (16) voor dit concert speciaal een compositie schrijven, die op deze avond voor het eerst te horen is.
+              Door muziek uit verschillende periodes en stijlen te combineren, ontstaat een veelzijdig geheel. Zo mogen de Vier Jaargetijden van Vivaldi natuurlijk niet ontbreken, maar wat dacht je bijvoorbeeld ook van het minder bekende Winter van Astor Piazzolla of Gershwin's Summertime uit Porgy and Bess? Daarnaast gaat de {' '}
+              <Link href='https://www.instagram.com/kobusjansenn/' isExternal textDecoration='underline'>
+                Kindercomponist des Vaderlands Kobus Jansen
+              </Link>{' '}
+              (16) voor dit concert speciaal een compositie schrijven, die op deze avond voor het eerst te horen is.
             </Text>
           </Box>
           <Box flex={1}>
@@ -44,13 +48,21 @@ export const Editie2026 = () => {
               Publieksprogramma
             </Heading>
             <Text as='span' textStyle={'paragraphBlock'}>
-              Tijdens de Kinder Klimaat Concert Dag is er heel veel ruimte voor publieksparticipatie. Er kunnen vragen worden gesteld bij de KlimaatHelpdesk, waarbij wetenschappers van de Universiteit Utrecht antwoord geven. Op deze manier ontmoeten hun belevingswereld en wetenschappelijke kennis elkaar.
+              Tijdens de Kinder Klimaat Concert Dag is er heel veel ruimte voor publieksparticipatie. Er kunnen vragen worden gesteld bij de{' '}
+              <Link href='https://klimaathelpdesk.org/' isExternal textDecoration='underline'>
+                KlimaatHelpdesk
+              </Link>
+              , waarbij wetenschappers van de Universiteit Utrecht antwoord geven. Op deze manier ontmoeten hun belevingswereld en wetenschappelijke kennis elkaar.
             </Text>
             <Text as='span' textStyle={'paragraphBlock'}>
               Er is ook een tekenwedstrijd, waarbij kinderen niet alleen hun favoriete seizoen gaan tekenen, maar ook worden gestimuleerd na te denken over wat zij mooi vinden aan de seizoenen, wat er mogelijk verandert en welke vragen of zorgen dat oproept. Tijdens dit proces ontvangen zij op een laagdrempelige manier informatie over natuur en seizoensverandering, passend bij hun leeftijd. De vragen en beelden die hieruit ontstaan krijgen een zichtbare plek binnen het programma. De tekeningen zijn vervolgens te zien in TivoliVredenburg, en de winnende tekeningen per seizoen worden geprojecteerd tijdens het concert. Al voor de dag zelf zijn er op tal van basisscholen in Nederland al tekeningen gemaakt ter voorbereiding op de dag, die op deze dag te zien zullen zijn.
             </Text>
             <Text as='span' textStyle={'paragraphBlock'}>
-              Na afloop presenteren middelbare scholieren uit de regio Utrecht hun onderzoek naar klimaatvraagstukken en gaan in gesprek met beleidsmakers, wetenschappers en publiek. Dit wordt mogelijk gemaakt door U-Talent, een regionaal netwerk waarin de Universiteit Utrecht, de Hogeschool Utrecht en circa vijftig middelbare scholen samenwerken. De Kinder Klimaat Concert Dag verbindt zo generaties rond luisteren, ontmoeten en reflectie.
+              Na afloop presenteren middelbare scholieren uit de regio Utrecht hun onderzoek naar klimaatvraagstukken en gaan in gesprek met beleidsmakers, wetenschappers en publiek. Dit wordt mogelijk gemaakt door{' '}
+              <Link href='https://u-talent.nl/' isExternal textDecoration='underline'>
+                U-Talent
+              </Link>
+              , een regionaal netwerk waarin de Universiteit Utrecht, de Hogeschool Utrecht en circa vijftig middelbare scholen samenwerken. De Kinder Klimaat Concert Dag verbindt zo generaties rond luisteren, ontmoeten en reflectie.
             </Text>
           </Box>
         </Box>
@@ -61,7 +73,11 @@ export const Editie2026 = () => {
           De Kinder Klimaat Concert Dag is meer dan alleen de dag zelf. In ruim een maand voorafgaand aan het evenement doorlopen de deelnemers een inhoudelijk uitdagend en creatief traject. Dit traject heeft tot doel artistieke en persoonlijke groei te stimuleren, evenals de verbinding tussen diverse groepen. De selectie van deelnemers wordt verzorgd door initiatiefnemers Luna en Donna van Leeuwen, in samenwerking met partners binnen talentontwikkeling. Zij stellen een diverse en inclusieve groep samen op basis van motivatie, talent en achtergrond.
         </Text>
         <Text as='span' textStyle={'paragraphBlock'}>
-          De deelnemende jonge musici gaan niet alleen artistiek aan de slag, maar doen ook een unieke podiumervaring op als geëngageerd kunstenaar. Tijdens de repetities worden zij voorbereid op hun maatschappelijke rol op het podium: hoe neem je als slagwerker of violist het publiek overtuigend mee? In samenwerking met de KlimaatHelpdesk worden de jongeren bevraagd over wat klimaatverandering en in het bijzonder de verandering van de seizoenen, voor hen betekent. Ze worden uitgenodigd hun eigen gevoel te verwoorden—zorg, hoop, maar ook ongemak. Wat als je liever niet aan het klimaat wilt denken? Alle input wordt meegenomen en verwerkt in de voorstelling. Zo ontstaat er, onder supervisie van onze creatief producent, een oprechte en authentieke boodschap die een breed spectrum aan
+          De deelnemende jonge musici gaan niet alleen artistiek aan de slag, maar doen ook een unieke podiumervaring op als geëngageerd kunstenaar. Tijdens de repetities worden zij voorbereid op hun maatschappelijke rol op het podium: hoe neem je als slagwerker of violist het publiek overtuigend mee? In samenwerking met de{' '}
+          <Link href='https://klimaathelpdesk.org/' isExternal textDecoration='underline'>
+            KlimaatHelpdesk
+          </Link>{' '}
+          worden de jongeren bevraagd over wat klimaatverandering en in het bijzonder de verandering van de seizoenen, voor hen betekent. Ze worden uitgenodigd hun eigen gevoel te verwoorden—zorg, hoop, maar ook ongemak. Wat als je liever niet aan het klimaat wilt denken? Alle input wordt meegenomen en verwerkt in de voorstelling. Zo ontstaat er, onder supervisie van onze creatief producent, een oprechte en authentieke boodschap die een breed spectrum aan
           emoties bespreekbaar maakt.
         </Text>
         <Text as='span' textStyle={'paragraphBlock'}>
@@ -71,7 +87,11 @@ export const Editie2026 = () => {
           De Kindercomponist der Nederlanden, Kobus Jansen, schrijft speciaal voor de Kinder Klimaat Concert Dag een korte compositie. Ter voorbereiding ontvangt hij vijf coachingsessies van gerenommeerde Nederlandse componisten. De jonge componist schrijft een compositie voor het seizoen "de lente". In een aantal sessies werkt hij stap voor stap aan de verdere ontwikkeling van zijn stuk. Voor Kobus is dit een bijzondere kans om zijn compositievaardigheden en creatieve talenten verder uit te bouwen. Als extra bekroning wordt zijn compositie uitgevoerd in een van de meest toonaangevende concertzalen van Nederland: TivoliVredenburg in Utrecht.
         </Text>
         <Text as='span' textStyle={'paragraphBlock'}>
-          Vanaf het begin van het cursusjaar 2026-2027 zullen basisschoolleerlingen van deelnemende scholen uit diverse regio's aan de slag gaan met de KKCD Tekenwedstrijd. Het doel is om kinderen uit alle lagen van de bevolking op een laagdrempelige manier bij het project te betrekken, ongeacht of ze een instrument bespelen of thuis iets meekrijgen over het klimaat. Op school krijgen zij een korte, leeftijdsgerichte uitleg over klimaatverandering, verwerkt in een lespakket dat niet polariserend is, maar juist gericht op oplossingen. De KlimaatHelpdesk wordt daarbij geïntroduceerd als vraagbaak voor hun vragen.
+          Vanaf het begin van het cursusjaar 2026-2027 zullen basisschoolleerlingen van deelnemende scholen uit diverse regio's aan de slag gaan met de KKCD Tekenwedstrijd. Het doel is om kinderen uit alle lagen van de bevolking op een laagdrempelige manier bij het project te betrekken, ongeacht of ze een instrument bespelen of thuis iets meekrijgen over het klimaat. Op school krijgen zij een korte, leeftijdsgerichte uitleg over klimaatverandering, verwerkt in een lespakket dat niet polariserend is, maar juist gericht op oplossingen. De{' '}
+          <Link href='https://klimaathelpdesk.org/' isExternal textDecoration='underline'>
+            KlimaatHelpdesk
+          </Link>{' '}
+          wordt daarbij geïntroduceerd als vraagbaak voor hun vragen.
         </Text>
         <Heading as='h3' size='l' mt={0} mb={4} color='#6f73b2'>
           Repetitieproces
